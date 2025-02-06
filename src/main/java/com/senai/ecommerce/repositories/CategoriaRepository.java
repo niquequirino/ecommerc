78538b4ea8cf93505f1bd26635199452974c2089
niquequirino <nique.quirino@gmail.com>
2025-02-06 08:41:23 -0300

    Continuação parte dois

--- src/main/java/com/senai/ecommerce/repositories/CategoriaRepository.java
+++ src/main/java/com/senai/ecommerce/repositories/CategoriaRepository.java
@@ -1,9 +1,11 @@
 package com.senai.ecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.senai.ecommerce.entities.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
 }

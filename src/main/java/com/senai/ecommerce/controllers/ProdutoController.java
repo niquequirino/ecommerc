2f89098d18f783157e63f40476808436922c930a
niquequirino <nique.quirino@gmail.com>
2025-02-11 17:18:53 -0300

    colocando post

--- src/main/java/com/senai/ecommerce/controllers/ProdutoController.java
+++ src/main/java/com/senai/ecommerce/controllers/ProdutoController.java
@@ -5,16 +5,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.senai.ecommerce.dto.ProdutoDTO;
+import com.senai.ecommerce.entities.Produto;
 import com.senai.ecommerce.services.ProdutoService;
 
 @RestController
-@RequestMapping(value = "/produto")
+@RequestMapping(value = "produto")
 public class ProdutoController {
 	
 	@Autowired
@@ -28,5 +32,12 @@ public class ProdutoController {
 	@GetMapping(value = "pagina")
 	public ResponseEntity<Page<ProdutoDTO>> buscarPagina(Pageable pagina){
 		return ResponseEntity.ok(service.buscarPagina(pagina));
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<ProdutoDTO> adicionar(@RequestBody ProdutoDTO produtoDTO) {
+		ProdutoDTO savedProduto = service.adicionar(produtoDTO);
+		
+		return ResponseEntity.ok(savedProduto);
 	}
 }
--- src/main/java/com/senai/ecommerce/services/ProdutoService.java
+++ src/main/java/com/senai/ecommerce/services/ProdutoService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.senai.ecommerce.dto.ProdutoDTO;
@@ -25,5 +26,19 @@ public class ProdutoService {
 		public Page<ProdutoDTO> buscarPagina(Pageable pagina){
 			Page<Produto> result = repo.findAll(pagina);
 			return result.map(x -> new ProdutoDTO(x));
+		}
+		
+		public ProdutoDTO adicionar (ProdutoDTO produtoDTO){
+			Produto produto = new Produto();
+			
+			produto.setNome(produtoDTO.getNome());
+			produto.setDescricao(produto.getDescricao());
+			produto.setPreco(produto.getPreco());
+			produto.setImgUrl(produto.getImgUrl());
+			
+			Produto saveProduto = repo.save(produto);
+			
+			return new ProdutoDTO(saveProduto);
+			
 		}
 }
